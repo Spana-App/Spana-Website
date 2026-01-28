8cf62f1b2fca9d58bc9587194ecc0a29150d5bd6
Xoli <xolinxiweni@gmail.com>
2026-01-28 13:49:34 +0200

    Spana Website Build

--- src/components/ui/Footer/Footer.jsx
+++ src/components/ui/Footer/Footer.jsx
@@ -349,9 +349,3 @@ const Footer = () => {
 export default Footer
 
 
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/Spana-App/Spana-Website.git
-git push -u origin main
